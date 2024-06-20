08087bdfaa2334ebb930b1eed59f3d82757c7211
Aman Dewett <amandewett@gmail.com>
2024-06-20 12:10:47 -0400

    Update HomeHeaderRight.tsx

--- components/home/HomeHeaderRight.tsx
+++ components/home/HomeHeaderRight.tsx
@@ -6,7 +6,7 @@ import Tilt from "react-parallax-tilt";
 const HomeHeaderRight = () => {
   return (
     <div className="w-[100%] px-10 ml-auto mr-auto">
-      <Tilt scale={1}>
+      <Tilt tiltReverse={true}>
         <LottieAnimation
           animationData={CodingLottieOne}
           className="ml-auto mr-auto p-0 z-10 xxlMonitor:w-[550px] xxlMonitor:h-[550px] m-0 xlMonitor:w-[450px] xlMonitor:h-[450px] lgMonitor:w-[400px] lgMonitor:h-[400px] laptop:w-[400px] laptop:h-[400px] tablet:w-[350px] tablet:h-[350px] phone:w-[300px] phone:h-[300px]"
