3395c0615722e40428454e3d97aaec8b6b654517
Aman Dewett <amandewett@gmail.com>
2024-09-05 06:46:47 -0400

    updates
    - added scroll icon on experience section, to let user know about horizontal scrolling or continue scrolling for more.

--- components/home/ScrollMessage.tsx
+++ components/home/ScrollMessage.tsx
@@ -1,16 +1,19 @@
 "use client";
-import { AnimatePresence, motion, useScroll, useTransform } from "framer-motion";
+import { AnimatePresence, motion, useMotionValueEvent, useScroll, useTransform } from "framer-motion";
 import { CiDesktopMouse2 } from "react-icons/ci";
 import AppLottieContainer from "../shared/AppLottieContainer";
 import { ArrowLottie } from "@/public";
+import { useEffect } from "react";
+import { ScrollMessageProps } from "@/utils/appTypes";
 
-const ScrollMessage = () => {
+const ScrollMessage = ({ arrScrollYPosition }: ScrollMessageProps) => {
   const { scrollY } = useScroll();
-  const opacityValue = useTransform(scrollY, [0, 30, 60, 90, 100], [1, 0.8, 0.5, 0.2, 0]);
+  const opacityValue = useTransform(scrollY, arrScrollYPosition ? arrScrollYPosition : [0, 30, 60, 90, 100], arrScrollYPosition ? [0, 1, 0].reverse() : [1, 0.8, 0.5, 0.2, 0]);
+
   return (
     <AnimatePresence initial>
       <motion.section
-        className="flex-col items-center hidden lg:flex fixed bottom-10 w-full"
+        className={`flex-col items-center ${arrScrollYPosition ? "flex" : "hidden"} lg:flex fixed bottom-10 w-full`}
         initial={{ y: -10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         style={{ opacity: opacityValue }}
