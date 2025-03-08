56d1fcc450047ab0d1c3b08734dcacc119bb73fd
Aman Dewett <amandewett@gmail.com>
2025-03-08 00:42:16 -0500

    updates
    - updated emals

--- components/home/SocialMediaComponent.tsx
+++ components/home/SocialMediaComponent.tsx
@@ -41,7 +41,7 @@ const SocialMediaComponent = () => {
         className="flex flex-row lg:flex-col lg:justify-center items-center space-x-2 lg:space-x-0 lg:space-y-5 text-3xl text-appLightTextColor"
       >
         <motion.li variants={item} className="w-[6rem] h-[0.3rem] lg:w-[0.3rem] lg:h-[6rem] bg-appDarkBgColor dark:bg-appLightBgColor" />
-        <motion.a href="mailto:connect@dewett.ca?subject=Requirement" target="_blank" variants={item} className="bg-[#A2D2FFCC] hover:bg-[#A2D2FF] transition-colors duration-300 rounded-[50%] p-2">
+        <motion.a href="mailto:contact@dewett.ca?subject=Requirement" target="_blank" variants={item} className="bg-[#A2D2FFCC] hover:bg-[#A2D2FF] transition-colors duration-300 rounded-[50%] p-2">
           <MdEmail className="size-[1.2rem] md:size-[1.3rem] xl:size-[1.5rem]" />
         </motion.a>
         <motion.a href="https://github.com/amandewett" target="_blank" variants={item} className="bg-[#BDB2FFCC] hover:bg-[#BDB2FF] transition-colors duration-300 rounded-[50%] p-2">
